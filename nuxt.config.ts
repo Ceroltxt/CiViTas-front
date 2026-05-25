a9f617bd2e2e59db2d0d70c1b8105268af411ced
Ceroltxt <carolramiro123@gmail.com>
2026-05-24 22:02:54 -0300

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,19 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+
+  modules: ['@pinia/nuxt', '@nuxt/eslint'],
+
+  runtimeConfig: {
+    jwtCookieName: process.env.NUXT_JWT_COOKIE_NAME,
+    public: {
+      appName: process.env.NUXT_PUBLIC_APP_NAME,
+      apiBase: process.env.NUXT_PUBLIC_API_BASE,
+    },
+  },
+
+  nitro: {
+    preset: 'vercel',
+  },
+})
